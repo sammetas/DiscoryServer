c1ab0a84bb5061e2f077627a6bef888f00835c21
sammetas <raamaalayam@gmail.com>
2022-05-01 19:32:44 +0700

    Test3: Test case 3 added

---
+++ src/test/java/com/samm/DiscoryServer/Test3.java
@@ -0,0 +1,19 @@
+package com.samm.DiscoryServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.SQLOutput;
+
+@SpringBootTest
+class Test3 {
+
+    @Test
+    void contextLoads() {
+    }
+
+     @Test
+    void test3(){
+         System.out.println("Test 3 world!!");
+     }
+}
