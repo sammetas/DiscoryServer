70e6d912e4c179cb021b75d6f6951abd400d27b1
sammetas <raamaalayam@gmail.com>
2022-05-01 20:14:52 +0700

    Rebase: test added

---
+++ src/test/java/com/samm/DiscoryServer/RebaseTest.java
@@ -0,0 +1,10 @@
+package com.samm.DiscoryServer;
+
+import org.junit.jupiter.api.Test;
+
+public class RebaseTest {
+    @Test
+    void rebaseTest(){
+        System.out.println("Rebaseds");
+    }
+}
